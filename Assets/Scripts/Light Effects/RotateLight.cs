7ecc08cea5cde50e1003417a34a7910adcd030ca
MichaelxhJiang <michaelxhj@outlook.com>
2016-04-11 21:40:38 -0400

    updated lights

--- Assets/Scripts/Light Effects/RotateLight.cs
+++ Assets/Scripts/Light Effects/RotateLight.cs
@@ -2,10 +2,22 @@
 using System.Collections;
 
 public class RotateLight : MonoBehaviour {
-	
+
+	private int rotation;
+	private float rotateSpeed;
+	void Start () {
+		if (Random.Range (0.0f, 1.0f) < 0.5f) {
+			rotation = 1;
+		} else {
+			rotation = -1;
+		}
+
+		rotateSpeed = 50 * Random.Range (1.0f, 1.5f);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		gameObject.transform.Translate(0.0f, Time.deltaTime * 10.0f, 0.0f, Space.Self); 
-		gameObject.transform.Rotate ( new Vector3 (0.0f, Time.deltaTime * 50.0f, 0.0f), Space.World);
+		gameObject.transform.Rotate ( new Vector3 (0.0f, Time.deltaTime * rotateSpeed * rotation, 0.0f), Space.World);
 	}
 }
